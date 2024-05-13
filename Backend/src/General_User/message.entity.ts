a2023e56c809ae7c9656346eaa2c81a37097bb04
Maishara <148905254+Maishara@users.noreply.github.com>
2024-05-14 03:14:51 +0600

    Update message.entity.ts

--- Backend/src/General_User/message.entity.ts
+++ Backend/src/General_User/message.entity.ts
@@ -9,6 +9,15 @@ export class MessageEntity {
     @Column()
     content: string;
 
-    @ManyToOne(() => UserEntity, user => user.messages)
-    user: UserEntity;
+    @ManyToOne(() => UserEntity, user => user.sentMessages) // Assuming you have a 'sentMessages' relationship in your UserEntity
+    sender: UserEntity;
+
+    @ManyToOne(() => UserEntity, user => user.receivedMessages) // Assuming you have a 'receivedMessages' relationship in your UserEntity
+    recipient: UserEntity;
+
+    @Column()
+    recipientId: number;
+
+    @Column()
+    senderId: number;
 }
