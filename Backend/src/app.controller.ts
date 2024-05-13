813b47b2295814dd46b5a3e82a4867307e354c66
Maishara <148905254+Maishara@users.noreply.github.com>
2024-05-14 03:09:50 +0600

    Updated messaging system  app.controller.ts

--- Backend/src/app.controller.ts
+++ Backend/src/app.controller.ts
@@ -1,21 +1,39 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, NotFoundException, Req, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessageDto } from './message.dto';
 import { JwtAuthGuard } from './auth/jwt.auth-guard'; 
+import { UserService } from './General_User/guser.service';
+import { MessageEntity } from './General_User/message.entity';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  
+  constructor(private readonly appService: AppService, private readonly buserService: UserService
+
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Get('messages')
-  async getMessages() {
-    return this.appService.getMessages();
+  async getMessages(@Req() req: any) {
+    const userId = req.user.id;
+    return this.appService.getMessages(userId);
   }
+
   @UseGuards(JwtAuthGuard)
   @Post('messages')
-  async sendMessage(@Body() messageDto: MessageDto) {
+  async sendMessage(@Req() req: any, @Body() messageDto: MessageDto) {
+    const senderId = req.user.id;
+    
+    // Assign senderId directly from the user object in the request
+    messageDto.senderId = senderId;
+    
+    // Call the service method to send the message
     return this.appService.sendMessage(messageDto);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('messages/:id')
+  async getMessageById(@Param('id') id: number): Promise<MessageEntity> {
+    return this.appService.getMessageById(id);
   }
 
  
